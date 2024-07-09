155d414c541eeda3af55f66c328f309afaf5a650
KDnul <kelvin272duo@gmail.com>
2024-07-09 14:40:23 -0700

    Added SQL query classes for login, appointments, and users

--- src/main/java/kduongmain/controller/CustomerAddController.java
+++ src/main/java/kduongmain/controller/CustomerAddController.java
@@ -1,4 +1,48 @@
 package kduongmain.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
 public class CustomerAddController {
+
+    @FXML
+    private TextField addCustomerAddressTxt;
+
+    @FXML
+    private Button addCustomerCancelBtn;
+
+    @FXML
+    private ComboBox<?> addCustomerCountryCB;
+
+    @FXML
+    private ComboBox<?> addCustomerDivisionCB;
+
+    @FXML
+    private TextField addCustomerIdTxt;
+
+    @FXML
+    private TextField addCustomerNameTxt;
+
+    @FXML
+    private TextField addCustomerPhoneNumberTxt;
+
+    @FXML
+    private TextField addCustomerPostalTxt;
+
+    @FXML
+    private Button addCustomerSaveBtn;
+
+    @FXML
+    void onAddCustomerCancelBtnClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onAddCustomerSaveBtnClicked(ActionEvent event) {
+
+    }
+
 }
--- src/main/java/kduongmain/controller/CustomerViewController.java
+++ src/main/java/kduongmain/controller/CustomerViewController.java
@@ -1,4 +1,67 @@
 package kduongmain.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class CustomerViewController {
+
+    @FXML
+    private Button customerAddBtn;
+
+    @FXML
+    private TableColumn<?, ?> customerAddressCol;
+
+    @FXML
+    private Button customerBackBtn;
+
+    @FXML
+    private TableColumn<?, ?> customerCountryCol;
+
+    @FXML
+    private Button customerDelBtn;
+
+    @FXML
+    private TableColumn<?, ?> customerDivisionCol;
+
+    @FXML
+    private TableColumn<?, ?> customerIdCol;
+
+    @FXML
+    private Button customerModBtn;
+
+    @FXML
+    private TableColumn<?, ?> customerNameCol;
+
+    @FXML
+    private TableColumn<?, ?> customerPhoneNumberCol;
+
+    @FXML
+    private TableColumn<?, ?> customerPostalCodeCol;
+
+    @FXML
+    private TableView<?> customerTableVIew;
+
+    @FXML
+    void onCustomerAddBtnClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onCustomerBackBtnClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onCustomerDelBtnClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onCustomerModBtnClicked(ActionEvent event) {
+
+    }
+
 }
